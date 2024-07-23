1ac6841671a6a551c016a5456f16aef4472c7888
Hernan <hernanariza69@gmail.com>
2024-07-22 22:31:26 -0500

    feat: :sparkles: Main uploaded!

--- src/main/java/com/mycompany/work/Work.java
+++ src/main/java/com/mycompany/work/Work.java
@@ -2,6 +2,7 @@ package com.mycompany.work;
 
 import com.mycompany.work.Database.CConexion;
 import java.sql.Connection;
+import java.sql.SQLException;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -13,11 +14,15 @@ import java.sql.Connection;
  * @author hernan
  */
 
-
 public class Work {
     public static void main(String[] args) {
+        
         CConexion objetoConexion = new CConexion();
         objetoConexion.estabableceConexion();
+        
+        Menu menu = new Menu();
+        menu.mostrarMenu();
+        
         System.out.println("Hello world!");
    }
 }
