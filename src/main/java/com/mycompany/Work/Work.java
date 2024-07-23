f95bf00b3c9a5c0bbd49e5de52a3ac408b5ebd2a
Hernan <hernanariza69@gmail.com>
2024-07-23 15:33:43 -0500

    feat: :sparkles: interfas user create camper

---
+++ src/main/java/com/mycompany/Work/Work.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.work;
+
+
+/**
+ *
+ * @author hernan
+ */
+
+/**
+ *
+ * @author hernan
+ */
+public class Work {
+
+    public static void main(String[] args) {
+        
+        JavaView view1 = new JavaView();
+        view1.setVisible(true);
+        
+        System.out.println("Hello World!");
+        
+    }
+}
+
