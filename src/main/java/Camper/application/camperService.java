c8b94116cb7cbf08ebacb3e82f5a7a1bc585a464
Hernan <hernanariza69@gmail.com>
2024-07-23 07:24:20 -0500

    feat: :sparkles: Camper functions!

---
+++ src/main/java/Camper/application/camperService.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Camper.application;
+
+import Camper.domain.models.Camper;
+import Camper.infrastructure.camperRepository;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ *
+ * @author hernan
+ */
+public class camperService {
+    
+    private final camperRepository CamperRepository;
+    
+    public camperService(camperRepository CamperRepository) {
+        this.CamperRepository = CamperRepository;
+    }
+    
+    public void createCamper(Camper camper) {
+        CamperRepository.save(camper);
+    }
+    
+    public void updateCamper(Camper camper) {
+        CamperRepository.update(camper);
+    }
+    
+    public Optional<Camper> getCamperById(int id) {
+        return CamperRepository.findById(id);
+    }
+    
+    public void deletePais(int id) {
+        CamperRepository.delete(id);
+    }
+    
+    public List<Camper> getAllCampers() {
+        return CamperRepository.findAll();
+    }
+}
