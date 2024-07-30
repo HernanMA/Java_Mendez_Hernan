5eddcc7eaf8a12b7b7137042d93bafb066177755
Hernan <hernanariza69@gmail.com>
2024-07-29 19:39:12 -0500

    feat: :sparkles:

---
+++ Hospital/src/main/java/com/mycompany/hospital/Hospital.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hospital;
+
+/**
+ *
+ * @author hernan
+ */
+public class Hospital {
+
+    public static void main(String[] args) {
+        
+        Hospi hospi = new Hospi();
+        hospi.setVisible(true);
+        
+        System.out.println("Hello World!");
+    }
+}
