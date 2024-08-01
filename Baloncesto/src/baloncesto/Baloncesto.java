9995fcadec2d6a9eebee6d5354811d270a1d23a2
Hernan <hernanariza69@gmail.com>
2024-08-01 16:36:55 -0500

    feat: :sparkles: UI

--- Baloncesto/src/baloncesto/Baloncesto.java
+++ Baloncesto/src/baloncesto/Baloncesto.java
@@ -14,7 +14,10 @@ public class Baloncesto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Menu menu = new Menu();
+        menu.setVisible(true);
+        
     }
     
 }
