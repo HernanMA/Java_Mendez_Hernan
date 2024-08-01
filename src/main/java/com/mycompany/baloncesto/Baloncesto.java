ce5058258a9f087a6c26037ee19561a786ffa764
Hernan <hernanariza69@gmail.com>
2024-08-01 07:41:18 -0500

    feat: :sparkles:

---
+++ src/main/java/com/mycompany/baloncesto/Baloncesto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.baloncesto;
+
+/**
+ *
+ * @author hernan
+ */
+public class Baloncesto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
