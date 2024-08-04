2da16cca3389d34d9907b872c001b9ff789da45a
Hernan <hernanariza69@gmail.com>
2024-08-03 19:59:41 -0500

    feat: :sparkles: ..

--- Baloncesto/src/baloncesto/Baloncesto.java
+++ Baloncesto/src/baloncesto/Baloncesto.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package baloncesto;
 
@@ -9,15 +9,7 @@ package baloncesto;
  * @author hernan
  */
 public class Baloncesto {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         
-        Menu menu = new Menu();
-        menu.setVisible(true);
-        
     }
-    
 }
