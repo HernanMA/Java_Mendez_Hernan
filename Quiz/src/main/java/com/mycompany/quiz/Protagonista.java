9c4487ab653a4537bb7e9145fbe6a1413f652599
Hernan <hernanariza69@gmail.com>
2024-08-12 09:03:02 -0500

    feat: :sparkles:

---
+++ Quiz/src/main/java/com/mycompany/quiz/Protagonista.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.quiz;
+
+/**
+ *
+ * @author hernan
+ */
+public abstract class Protagonista {
+    public abstract void defender();
+    public abstract void atacar();
+}
