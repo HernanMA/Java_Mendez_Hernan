31ebf0414a48a2be3e2601b379042cc5f79f4fb2
Hernan <hernanariza69@gmail.com>
2024-08-16 08:35:36 -0500

    feat: :sparkles: Ejercicio

---
+++ FilaVirtual/src/filavirtual/ConcertBooking.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package filavirtual;
+
+/**
+ *
+ * @author hernan
+ */
+class Concert {
+    private int ticketsAvailable = 10;  // Número de entradas disponibles
+
+    public synchronized boolean sellTicket(String buyer) {
+        if (ticketsAvailable > 0) {
+            System.out.println(buyer + " compró una entrada. Entradas restantes: " + (ticketsAvailable - 1));
+            ticketsAvailable--;
+            return true;
+        } else {
+            System.out.println(buyer + " intentó comprar una entrada, pero ya no hay disponibles.");
+            return false;
+        }
+    }
+}
+
+class TicketSeller implements Runnable {
+    private Concert concert;
+    private String buyerName;
+
+    public TicketSeller(Concert concert, String buyerName) {
+        this.concert = concert;
+        this.buyerName = buyerName;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            if (!concert.sellTicket(buyerName)) {
+                break;  // Si ya no hay entradas, salir del bucle
+            }
+            try {
+                Thread.sleep(100);  // Simula el tiempo de procesamiento de la compra
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+public class ConcertBooking {
+    public static void main(String[] args) {
+        Concert concert = new Concert();
+
+        // Creando 15 compradores para simular la sobreventa
+        Thread buyer1 = new Thread(new TicketSeller(concert, "Comprador 1"));
+        Thread buyer2 = new Thread(new TicketSeller(concert, "Comprador 2"));
+        Thread buyer3 = new Thread(new TicketSeller(concert, "Comprador 3"));
+        Thread buyer4 = new Thread(new TicketSeller(concert, "Comprador 4"));
+        Thread buyer5 = new Thread(new TicketSeller(concert, "Comprador 5"));
+        Thread buyer6 = new Thread(new TicketSeller(concert, "Comprador 6"));
+        Thread buyer7 = new Thread(new TicketSeller(concert, "Comprador 7"));
+        Thread buyer8 = new Thread(new TicketSeller(concert, "Comprador 8"));
+        Thread buyer9 = new Thread(new TicketSeller(concert, "Comprador 9"));
+        Thread buyer10 = new Thread(new TicketSeller(concert, "Comprador 10"));
+        Thread buyer11 = new Thread(new TicketSeller(concert, "Comprador 11"));
+        Thread buyer12 = new Thread(new TicketSeller(concert, "Comprador 12"));
+        Thread buyer13 = new Thread(new TicketSeller(concert, "Comprador 13"));
+        Thread buyer14 = new Thread(new TicketSeller(concert, "Comprador 14"));
+        Thread buyer15 = new Thread(new TicketSeller(concert, "Comprador 15"));
+
+        // Iniciando los hilos
+        buyer1.start();
+        buyer2.start();
+        buyer3.start();
+        buyer4.start();
+        buyer5.start();
+        buyer6.start();
+        buyer7.start();
+        buyer8.start();
+        buyer9.start();
+        buyer10.start();
+        buyer11.start();
+        buyer12.start();
+        buyer13.start();
+        buyer14.start();
+        buyer15.start();
+    }
+}
---
+++ FilaVirtual/src/filavirtual/FilaVirtual.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package filavirtual;
+
+/**
+ *
+ * @author hernan
+ */
+public class FilaVirtual {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+}
+
+
